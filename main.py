59e7ddd8c57514fd7a4ee623ce6210b43c174334
Purushotham Dasari <punith.purushotham.dasari@gmail.com>
2025-07-03 21:09:58 +0530

    KeyError Exception Handling

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-nato_phonetic_list = [nato_phonetic_dict[letter] for letter in word]
-print(nato_phonetic_list)
+def create_nato_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        nato_phonetic_list = [nato_phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in alphabets are allowed. Please kindly enter a valid word.")
+        create_nato_phonetic()
+    else:
+        print(nato_phonetic_list)
+create_nato_phonetic()
